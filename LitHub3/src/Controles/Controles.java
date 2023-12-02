f1f9146b2e2a68a481f87cd25953007b8ec0ba2c
Aracelly126 <araguangasig95@gmail.com>
2023-12-02 11:50:14 -0500

    se agrego controles de contraseña

--- LitHub3/src/Controles/Controles.java
+++ LitHub3/src/Controles/Controles.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Controles;
 
+import java.awt.Color;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.JToggleButton;
+import javax.swing.border.LineBorder;
 
 public class Controles {
 
@@ -60,6 +64,55 @@ public class Controles {
             return false;
         }
         return true;
+    }
+    
+    public void colorBackgroundCmbx(JComboBox cmb, Color colorBad, Color colorGod) {
+        cmb.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                cmb.setBackground(colorGod);
+            }
+        });
+        cmb.setBackground(colorBad);
+    }
+
+    public String claveToString(JPasswordField p) {
+        char[] passwordChars = p.getPassword();
+        String password = new String(passwordChars);
+        return password;
+    }
+
+    public void colorBorderTxt(JTextField txt, Color colorBad, Color colorGod) {
+        txt.addKeyListener(new java.awt.event.KeyAdapter() {
+            @Override
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txt.setBorder(new LineBorder(colorGod, 3));
+            }
+        });
+        txt.setBorder(new LineBorder(colorBad, 3));
+    }
+
+    public void colorBackgroundTxt(JTextField txt, Color colorBad, Color colorGod) {
+        txt.addKeyListener(new java.awt.event.KeyAdapter() {
+            @Override
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txt.setBackground(colorGod);
+            }
+        });
+        txt.setBackground(colorBad);
+    }
+
+    public void crearVerPassword(JPasswordField pas, JToggleButton btn) {
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JToggleButton btn = (JToggleButton) e.getSource();
+                if (btn.isSelected()) {
+                    pas.setEchoChar((char) 0); // Mostrar texto sin ocultarlo
+                } else {
+                    pas.setEchoChar('\u2022'); // Ocultar texto
+                }
+            }
+        });
     }
 
   
