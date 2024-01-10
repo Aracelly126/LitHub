0e4c974bc8ab64bb5b980400b0fa6f9ee12a04b6
Aracelly126 <146950485+Aracelly126@users.noreply.github.com>
2024-01-10 18:05:30 -0500

    copiaSeguridadRestaurada2

--- LitHub3/src/Clases/Libro.java
+++ LitHub3/src/Clases/Libro.java
@@ -1,156 +1,71 @@
 package Clases;
 
-import java.awt.Image;
-import javax.swing.ImageIcon;
-
 public class Libro {
 
-<<<<<<< HEAD
-    private String titulo;
-    private ImageIcon image;
-    private String sinopsis;
-=======
     private String codigo;
     private String CORREO_USU;
     private String nombre;
->>>>>>> copiaSeguridad
     private String genero;
-    private int nPaginas;
-    private int codigoPdf;
-    private int clave;
-    private String nombrePdf;
-    byte[] archivoPdf;
+    private int numPag;
+    private String prestado;
 
-    public int getClave() {
-        return this.clave;
+    public Libro() {
     }
 
-<<<<<<< HEAD
-    public void setClave(int clave) {
-        this.clave = clave;
-    }
-
-    public Libro(String titulo, String rutaImagen, String sinopsis, int anchoPanel, int altoPanel, String genero, int nPaginas) {
-        this.titulo = titulo;
-        this.sinopsis = sinopsis;
-        this.image = redimensionarImagen("/imagenes/" + rutaImagen, anchoPanel, altoPanel);
-=======
     public Libro(String codigo, String correo_usu, String nombre, String genero, int numPag, String prestado) {
         this.codigo = codigo;
         this.CORREO_USU = correo_usu;
         this.nombre = nombre;
->>>>>>> copiaSeguridad
         this.genero = genero;
-        this.nPaginas = nPaginas;
-        this.codigoPdf = nPaginas;
+        this.numPag = numPag;
+        this.prestado = prestado;
     }
 
-    public Libro(ImageIcon image, byte[] archivoPdf) {
-        this.image = image;
-        this.archivoPdf = archivoPdf;
-    }
-    
-
-    public Libro(String sinopsis, String genero, int numPags, int codigoPdf, String nombrePdf, byte[] archivopdf, int clave) {
-        this.titulo = nombrePdf;
-        this.sinopsis = sinopsis;
-        this.genero = genero;
-        this.nPaginas = numPags;
-        this.codigoPdf = codigoPdf;
-        this.nombrePdf = nombrePdf;
-        this.archivoPdf = archivopdf;
-        this.clave = clave;
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
     }
 
-    public byte[] getArchivoPdf() {
-        return this.archivoPdf;
+    public String getCodigo() {
+        return this.codigo;
     }
 
-<<<<<<< HEAD
-    public void setArchivoPdf(byte[] archivoPdf) {
-        this.archivoPdf = archivoPdf;
-    }
-
-    public int getCodigoPdf() {
-        return this.codigoPdf;
-=======
     public void setAutor(String autor) {
         this.CORREO_USU = autor;
     }
 
     public String getCorreoUsu() {
         return this.CORREO_USU;
->>>>>>> copiaSeguridad
-    }
-
-    public void setCodigoPdf(int codigoPdf) {
-        this.codigoPdf = codigoPdf;
-    }
-
-    public String getNombrePdf() {
-        return this.nombrePdf;
-    }
-
-    public void setNombrePdf(String nombrePdf) {
-        this.nombrePdf = nombrePdf;
-    }
-
-    public ImageIcon getImage() {
-        return this.image;
-    }
-
-    public void setImage(ImageIcon image) {
-        this.image = image;
-    }
-
-    public String getNombre() {
-        return this.titulo;
     }
 
     public void setNombre(String nombre) {
-        this.titulo = nombre;
+        this.nombre = nombre;
     }
 
-    public String getTitulo() {
-        return this.titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getSinopsis() {
-        return this.sinopsis;
-    }
-
-    public void setSinopsis(String sinopsis) {
-        this.sinopsis = sinopsis;
-    }
-
-    public String getGenero() {
-        return this.genero;
+    public String getNombre() {
+        return this.nombre;
     }
 
     public void setGenero(String genero) {
         this.genero = genero;
     }
 
-    public int getnPaginas() {
-        return this.nPaginas;
+    public String getGenero() {
+        return this.genero;
     }
 
-    public void setnPaginas(int nPaginas) {
-        this.nPaginas = nPaginas;
+    public void setNumPag(int numPag) {
+        this.numPag = numPag;
     }
 
-    private ImageIcon redimensionarImagen(String rutaImagen, int ancho, int alto) {
-        ImageIcon imagenIcon = new ImageIcon(rutaImagen);
-        Image imagen = imagenIcon.getImage();
-        Image imagenRedimensionada = imagen.getScaledInstance(ancho, alto, Image.SCALE_SMOOTH);
-        if (imagenRedimensionada == null) {
-            System.out.println("Sin imagen");
-        }
-        return new ImageIcon(imagenRedimensionada);
+    public int getNumPag() {
+        return this.numPag;
     }
 
+    public void setPrestado(String prestado) {
+        this.prestado = prestado;
+    }
+
+    public String getPrestado() {
+        return this.prestado;
+    }
 }
--- LitHub3/src/Vista/frmregistro.java
+++ LitHub3/src/Vista/frmregistro.java
@@ -1,15 +1,5 @@
 package Vista;
 
-<<<<<<< HEAD
-import Almacen.Almacen;
-import Controles.ControlUsuarios;
-import Controles.Control_Lector;
-import java.awt.BorderLayout;
-import java.io.UnsupportedEncodingException;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-=======
 import BaseDatos.Almacen;
 import BaseDatos.Conexion;
 import Clases.Usuario;
@@ -18,274 +8,12 @@ import Utilidades.GestorPrograma;
 import Utilidades.ManejoComp;
 import java.awt.Color;
 import javax.swing.JOptionPane;
->>>>>>> copiaSeguridad
 
-/**
- *
- * @author thexe
- */
 public class frmregistro extends javax.swing.JFrame {
 
-    Almacen almaPermisos = new Almacen();
-    Control_Lector contLector = new Control_Lector();
+    private Conexion con = new Conexion();
 
     public frmregistro() {
-
-        initComponents();
-        this.setLocationRelativeTo(null);
-    }
-
-    @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
-
-        PanelUsuario = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
-        label_nombre = new javax.swing.JLabel();
-        textnombre = new javax.swing.JTextField();
-        label_correo = new javax.swing.JLabel();
-        textcorreo = new javax.swing.JTextField();
-        label_contra = new javax.swing.JLabel();
-        textcontra = new javax.swing.JPasswordField();
-        label_confirmar = new javax.swing.JLabel();
-        textconfirmar = new javax.swing.JPasswordField();
-        btnregistrar = new javax.swing.JButton();
-        label_direccion = new javax.swing.JLabel();
-        textdireccion = new javax.swing.JTextField();
-        label_telefono = new javax.swing.JLabel();
-        texttelefono = new javax.swing.JTextField();
-        label_inicio = new javax.swing.JLabel();
-        label_autor = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        imagen = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        fondo = new javax.swing.JLabel();
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        PanelUsuario.setBackground(new java.awt.Color(255, 255, 255));
-
-        jLabel2.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(51, 51, 51));
-        jLabel2.setText("Registro de Usuario");
-
-        label_nombre.setText("Nombre:");
-
-        textnombre.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-
-        label_correo.setText("Correo:");
-
-        textcorreo.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-
-        label_contra.setText("Contraseña:");
-
-        textcontra.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        textcontra.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                textcontraActionPerformed(evt);
-            }
-        });
-
-        label_confirmar.setText("Confirmar contraseña:");
-
-        textconfirmar.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        textconfirmar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                textconfirmarActionPerformed(evt);
-            }
-        });
-
-        btnregistrar.setText("Registrarse");
-        btnregistrar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        btnregistrar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnregistrarActionPerformed(evt);
-            }
-        });
-
-        label_direccion.setText("Direccion:");
-
-        textdireccion.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-
-        label_telefono.setText("Telefono:");
-
-        texttelefono.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-
-        label_inicio.setText("Ya tienes una cuenta?");
-
-        label_autor.setText("Eres autor?");
-
-        jButton1.setForeground(new java.awt.Color(51, 0, 102));
-        jButton1.setText("Registrarse como autor");
-        jButton1.setBorder(null);
-        jButton1.setContentAreaFilled(false);
-        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-
-        jButton2.setForeground(new java.awt.Color(51, 0, 102));
-        jButton2.setText("iniciar sesion");
-        jButton2.setBorder(null);
-        jButton2.setContentAreaFilled(false);
-        jButton2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout PanelUsuarioLayout = new javax.swing.GroupLayout(PanelUsuario);
-        PanelUsuario.setLayout(PanelUsuarioLayout);
-        PanelUsuarioLayout.setHorizontalGroup(
-            PanelUsuarioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(PanelUsuarioLayout.createSequentialGroup()
-                .addGap(150, 150, 150)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(PanelUsuarioLayout.createSequentialGroup()
-                .addGap(140, 140, 140)
-                .addComponent(label_nombre)
-                .addGap(82, 82, 82)
-                .addComponent(label_telefono))
-            .addGroup(PanelUsuarioLayout.createSequentialGroup()
-                .addGap(140, 140, 140)
-                .addComponent(textnombre, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(10, 10, 10)
-                .addComponent(texttelefono, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(PanelUsuarioLayout.createSequentialGroup()
-                .addGap(140, 140, 140)
-                .addComponent(label_correo))
-            .addGroup(PanelUsuarioLayout.createSequentialGroup()
-                .addGap(140, 140, 140)
-                .addComponent(textcorreo, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(PanelUsuarioLayout.createSequentialGroup()
-                .addGap(140, 140, 140)
-                .addComponent(label_direccion))
-            .addGroup(PanelUsuarioLayout.createSequentialGroup()
-                .addGap(140, 140, 140)
-                .addComponent(textdireccion, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(PanelUsuarioLayout.createSequentialGroup()
-                .addGap(140, 140, 140)
-                .addComponent(label_contra))
-            .addGroup(PanelUsuarioLayout.createSequentialGroup()
-                .addGap(140, 140, 140)
-                .addComponent(textcontra, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(PanelUsuarioLayout.createSequentialGroup()
-                .addGap(140, 140, 140)
-                .addComponent(label_confirmar))
-            .addGroup(PanelUsuarioLayout.createSequentialGroup()
-                .addGap(140, 140, 140)
-                .addComponent(textconfirmar, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(PanelUsuarioLayout.createSequentialGroup()
-                .addGap(210, 210, 210)
-                .addComponent(btnregistrar))
-            .addGroup(PanelUsuarioLayout.createSequentialGroup()
-                .addGap(30, 30, 30)
-                .addComponent(label_inicio)
-                .addGap(5, 5, 5)
-                .addComponent(jButton2))
-            .addGroup(PanelUsuarioLayout.createSequentialGroup()
-                .addGap(30, 30, 30)
-                .addComponent(label_autor)
-                .addGap(4, 4, 4)
-                .addComponent(jButton1))
-        );
-        PanelUsuarioLayout.setVerticalGroup(
-            PanelUsuarioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(PanelUsuarioLayout.createSequentialGroup()
-                .addGap(30, 30, 30)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(30, 30, 30)
-                .addGroup(PanelUsuarioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(label_nombre)
-                    .addComponent(label_telefono))
-                .addGap(4, 4, 4)
-                .addGroup(PanelUsuarioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(textnombre, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(texttelefono, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(10, 10, 10)
-                .addComponent(label_correo)
-                .addGap(4, 4, 4)
-                .addComponent(textcorreo, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(10, 10, 10)
-                .addComponent(label_direccion)
-                .addGap(4, 4, 4)
-                .addComponent(textdireccion, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(10, 10, 10)
-                .addComponent(label_contra)
-                .addGap(4, 4, 4)
-                .addComponent(textcontra, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(10, 10, 10)
-                .addComponent(label_confirmar)
-                .addGap(4, 4, 4)
-                .addComponent(textconfirmar, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(10, 10, 10)
-                .addComponent(btnregistrar)
-                .addGap(28, 28, 28)
-                .addGroup(PanelUsuarioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(label_inicio)
-                    .addComponent(jButton2))
-                .addGap(9, 9, 9)
-                .addGroup(PanelUsuarioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(label_autor)
-                    .addComponent(jButton1)))
-        );
-
-        getContentPane().add(PanelUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 0, 510, 576));
-
-        imagen.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/fondo_registro_1.png"))); // NOI18N
-        imagen.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 280, 540));
-
-        getContentPane().add(imagen, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 280, 540));
-
-        fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/fondo_login.jpg"))); // NOI18N
-        getContentPane().add(fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 790, 540));
-
-        pack();
-    }// </editor-fold>//GEN-END:initComponents
-
-    private void btnregistrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnregistrarActionPerformed
-        contLector.registroLectores(this.textnombre.getText(), this.texttelefono.getText(), this.textcorreo.getText(), this.textdireccion.getText(), this.textcontra.getText());
-    }//GEN-LAST:event_btnregistrarActionPerformed
-
-    private void textconfirmarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textconfirmarActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_textconfirmarActionPerformed
-
-    private void textcontraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textcontraActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_textcontraActionPerformed
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-        frmLogin login = new frmLogin();
-        login.setVisible(true);
-        this.setVisible(false);
-    }//GEN-LAST:event_jButton2ActionPerformed
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-        PanelAutor autor = new PanelAutor();
-        autor.setSize(510, 650);
-        autor.setLocation(0, 0);
-
-        this.PanelUsuario.removeAll();
-        this.PanelUsuario.add(autor, BorderLayout.CENTER);
-        this.PanelUsuario.revalidate();
-        this.PanelUsuario.repaint();
-    }//GEN-LAST:event_jButton1ActionPerformed
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
@@ -307,13 +35,12 @@ public class frmregistro extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(frmregistro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        initComponents();
+        this.setLocationRelativeTo(null);
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new frmregistro().setVisible(true);
-            }
-        });
+        ManejoComp.crearVerPassword(this.txt_contrasenia, this.btn_verContrasenia);
+        ManejoComp.crearVerPassword(this.txt_confContrasenia, this.btn_verConfContrasenia);
+        ManejoComp.crearlabel(this.lbl_izquierda, "SYSTEM/src/img_fondo2.png");
     }
     
     public void limpiarPanel(){
@@ -326,8 +53,6 @@ public class frmregistro extends javax.swing.JFrame {
         this.txt_confContrasenia.setText("");
     }
 
-<<<<<<< HEAD
-=======
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -578,29 +303,31 @@ public class frmregistro extends javax.swing.JFrame {
         ManejoComp.txtLongitudCondicion(this.txt_contrasenia, evt, 15);
     }//GEN-LAST:event_txt_confContraseniaKeyTyped
 
->>>>>>> copiaSeguridad
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    public javax.swing.JPanel PanelUsuario;
-    private javax.swing.JButton btnregistrar;
-    private javax.swing.JLabel fondo;
-    private javax.swing.JPanel imagen;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel label_autor;
-    private javax.swing.JLabel label_confirmar;
-    private javax.swing.JLabel label_contra;
-    private javax.swing.JLabel label_correo;
-    private javax.swing.JLabel label_direccion;
-    private javax.swing.JLabel label_inicio;
-    private javax.swing.JLabel label_nombre;
-    private javax.swing.JLabel label_telefono;
-    private javax.swing.JPasswordField textconfirmar;
-    private javax.swing.JPasswordField textcontra;
-    private javax.swing.JTextField textcorreo;
-    private javax.swing.JTextField textdireccion;
-    private javax.swing.JTextField textnombre;
-    private javax.swing.JTextField texttelefono;
+    public javax.swing.JButton btn_inicio;
+    private javax.swing.JButton btn_registrar;
+    private javax.swing.JToggleButton btn_verConfContrasenia;
+    private javax.swing.JToggleButton btn_verContrasenia;
+    private javax.swing.JComboBox<String> cmb_tipo;
+    private javax.swing.JLabel lbl_apellido;
+    private javax.swing.JLabel lbl_confContrasenia;
+    private javax.swing.JLabel lbl_contrasenia;
+    private javax.swing.JLabel lbl_correo;
+    private javax.swing.JLabel lbl_fechaNac;
+    private javax.swing.JLabel lbl_fondo;
+    private javax.swing.JLabel lbl_inicio;
+    private javax.swing.JLabel lbl_izquierda;
+    private javax.swing.JLabel lbl_nombre;
+    private javax.swing.JLabel lbl_pais;
+    private javax.swing.JLabel lbl_tipo;
+    private javax.swing.JLabel lbl_titulo;
+    public javax.swing.JPanel pnl_registro;
+    private javax.swing.JTextField txt_apellido;
+    private javax.swing.JPasswordField txt_confContrasenia;
+    private javax.swing.JPasswordField txt_contrasenia;
+    private javax.swing.JTextField txt_correo;
+    private com.toedter.calendar.JDateChooser txt_fecNac;
+    private javax.swing.JTextField txt_nombre;
+    private javax.swing.JTextField txt_pais;
     // End of variables declaration//GEN-END:variables
 }
